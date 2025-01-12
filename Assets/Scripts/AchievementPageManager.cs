aba81b59c00c68b45c3d88a0f01988bf35f9d4ed
kasjaduddin <kasjaduddin@outlook.com>
2025-01-12 15:14:07 +0700

    feat: add logic for main menu achievement page

---
+++ Assets/Scripts/AchievementPageManager.cs
@@ -0,0 +1,39 @@
+using ApplicationStorage;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AchievementPageManager : MonoBehaviour
+{
+    [Serializable] struct Achievement
+    {
+        [SerializeField] public GameObject unlocked;
+        [SerializeField] public GameObject locked;
+        [SerializeField] public GameObject name;
+    }
+
+    [SerializeField] private TextMeshProUGUI subtitle;
+    [SerializeField] private List<Achievement> achievement;
+
+    void OnEnable()
+    {
+        subtitle.text = "Achievement";
+        PlayerPrefsManager.DeleteAll();
+        for (int i = 0; i < 4; i++)
+        {
+            if (PlayerPrefsManager.GetInt("achievement" + (i + 1)) != 0)
+            {
+                achievement[i].unlocked.SetActive(true);
+                achievement[i].locked.SetActive(false);
+                achievement[i].name.SetActive(true);
+            }
+            else
+            {
+                achievement[i].unlocked.SetActive(false);
+                achievement[i].locked.SetActive(true);
+                achievement[i].name.SetActive(false);
+            }
+        }
+    }
+}
